237d15395478cfbba4f5e592ac07b9cb68125ce2
BurcovschiAna <burcovschi.ana2001@gmail.com>
2023-12-31 11:45:03 +0200

    Add files via upload

---
+++ Template 4/ex4.js
@@ -0,0 +1,23 @@
+"use strict";
+const hidden = document.querySelector(".hiden");
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+const btn4 = document.querySelector(".btn-4");
+const btn5 = document.querySelector(".btn-5");
+
+btn1.addEventListener("click", function () {
+  hidden.classList.toggle("hiden");
+});
+btn2.addEventListener("click", function () {
+  hidden.classList.toggle("hiden");
+});
+btn3.addEventListener("click", function () {
+  hidden.classList.toggle("hiden");
+});
+btn4.addEventListener("click", function () {
+  hidden.classList.toggle("hiden");
+});
+btn5.addEventListener("click", function () {
+  hidden.classList.toggle("hiden");
+});
